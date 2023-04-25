3c378604aeb35bfec7f47e4c341c76a0cfeed651
YYTWINT <yytwint.sisw@siemens.com>
2023-04-25 14:14:43 +0530

    Update DeployContainer.sh

--- shellScripts/DeployContainer.sh
+++ shellScripts/DeployContainer.sh
@@ -14,22 +14,28 @@ UNIT_PATH=$3
 
 STAGE_DIR=${STAGE_BASE_DIR}/TranslatorBinaries/
 
+INIT_DEF_FILE=${UNIT_PATH}/init.def
+stringarray=(`grep DMS_PARENT_BASELINE ${INIT_DEF_FILE} || { exit 1;}`)
+RELEASE_IP=${stringarray[1]}
+orig=${RELEASE_IP}
+IFS=. read -r nxVersion IP <<< ${RELEASE_IP}
+releaseName=${orig//'.'/'_TranslatorWorker.'}
+commonLocation=//plm/pnnas/ppic/Data_Exchange/SaaS_distributions/NXJT/${releaseName}/
+
 if [ ${EXECUTE_DEPLOY} == "true" ]
 then
-	INIT_DEF_FILE=${UNIT_PATH}/init.def
-	stringarray=(`grep DMS_PARENT_BASELINE ${INIT_DEF_FILE} || { exit 1;}`)
-	RELEASE_IP=${stringarray[1]}
-	echo "Deploy flag is set to true. Executing deploy step with release IP = ${RELEASE_IP}..."
-	
-	orig=${RELEASE_IP}
-	releaseName=${orig//'.'/'_TranslatorWorker.'}
-	
-	cd ${STAGE_BASE_DIR} || { exit 1;}
+	echo "Deploy flag is set to true. Executing deploy step with release IP =${RELEASE_IP} in Linux..."
+	cd ${commonLocation}/lnx64 || { exit 1;}
 	tar -czf $releaseName.tar.gz TranslatorBinaries/ || { exit 1;}
+	
 	echo "curl -u opentools_bot:YL6MtwZ35 -T $releaseName.tar.gz https://artifacts.industrysoftware.automation.siemens.com/artifactory/generic-local/Opentools/PREVIEW/NXtoJT/$releaseName/ || { exit 1;}"
+	
+	echo "curl -u opentools_bot:YL6MtwZ35 -T $releaseName.tar.gz https://artifacts.industrysoftware.automation.siemens.com/artifactory/generic-local/Opentools/PREVIEW/NXtoJT/$releaseName/">deployStep.txt
 
 	echo "curl -u opentools_bot:YL6MtwZ35 -T NXJT_Translator_README.txt https://artifacts.industrysoftware.automation.siemens.com/artifactory/generic-local/Opentools/PREVIEW/NXtoJT/$releaseName/ || { exit 1;}"
 
+	echo "curl -u opentools_bot:YL6MtwZ35 -T NXJT_Translator_README.txt https://artifacts.industrysoftware.automation.siemens.com/artifactory/generic-local/Opentools/PREVIEW/NXtoJT/$releaseName/">>deployStep.txt
+	
 	cd -
 else
 	echo "Deploy flag is set to false. Skipping deploy step..."
